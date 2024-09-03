f8631d68e8a44b32aedec3ae3be4a827f7e805fc
Alaa-Mostafaa <alan.mustafa.2002@gmail.com>
2024-09-03 16:47:43 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,23 +1,20 @@
-
 /* The arrow appears after it passes the navigation bar */
 $(window).scroll(function(){
     let windowScroll=$(window).scrollTop();
-
-
     if(windowScroll > 200){
-
-     
       $('#fixed-arrow').show(1000)
-
     }
     else{
         $('#fixed-arrow').hide(1000)
-
     }
   })
-
   $('#fixed-arrow').click(function(){
     $('html').animate({
       scrollTop:0
     },1000)
+  })
+  $(window).ready(()=>{
+    $('#loading').slideUp(3000,function(){
+      $('body').css('overflow-y','scroll')
+    })
   })
